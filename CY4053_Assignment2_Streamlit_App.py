a19bebedd67244c84951a1ef3a689d332406e063
`Muhammad Anas <i229808@nu.edu.pk>
2025-11-02 01:48:21 +0500

    CY4053_Assignment2_Streamlit_App.py

--- CY4053_Assignment2_Streamlit_App.py
+++ CY4053_Assignment2_Streamlit_App.py
@@ -501,11 +501,7 @@ def main():
     if "force_page" in st.session_state:
         page = st.session_state["force_page"]
         st.session_state.clear()
-        pass
-
-            # Clear session and show logout message
-            st.session_state.clear()
-            st.success("😔 You have been logged out successfully.")
+        st.success("😔 You have been logged out successfully.")
     else:
         page = st.sidebar.selectbox("Navigation", ["Home","Register","Login","Profile","Wallets","File Upload","Encryption Tool","Audit Logs","Export Testcases"])
 
